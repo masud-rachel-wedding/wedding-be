2cdd39d0281721bc2aac0ef038141364e683a520
awwwyeahmuffins <masud.zari@gmail.com>
2019-11-03 19:21:42 -0600

    submit route done

--- routes/index.js
+++ routes/index.js
@@ -30,12 +30,108 @@ router.post("/login", async function(req, res, next) {
 
 /* Save seriousness of party to respective row in database */
 router.post("/submitRSVP", async function(req, res, next) {
-  console.log(JSON.stringify(req.body));
+  const data = req.body;
+
+  /* Save to RSVP Table */
   try {
-    res.json({result: true});
-  } catch {
-    res.json({result: false});
+    const yesList = [];
+    const maybeList = [];
+    const noList = [];
+    for (const member of data.party.members) {
+      member.status === "coming" ? yesList.push(member.name) : "";
+      member.status === "maybe" ? maybeList.push(member.name) : "";
+      member.status === "probablyNot" ? noList.push(member.name) : "";
+    }
+
+    await pool.query(
+      `INSERT INTO wedding."RSVP" (code, coming, maybe, probablyNot) VALUES ('${data.code}','{${yesList}}','{${maybeList}}','{${noList}}')`
+    );
+    await pool.query("COMMIT");
+  } catch (error) {
+    console.log(error);
+    res.json({ result: false });
   }
+  /* Save Vote */
+  try {
+    let greeceBool = false;
+    let croatiaeBool = false;
+    if (data.countryVote === "greece") {
+      greeceBool = true;
+    } else {
+      croatiaeBool = true;
+    }
+
+    await pool.query(
+      `INSERT INTO wedding."CountryVote" (code, greece, croatia) VALUES ('${data.code}', ${greeceBool}, ${croatiaeBool})`
+    );
+    await pool.query("COMMIT");
+  } catch (error) {
+    console.log(error);
+    res.json({ result: false });
+  }
+  /* Conflict Opt Out*/
+  try {
+    const conflictOptOut = data.conflicts.conflictsOptOut;
+    console.log(conflictOptOut, "conflictOptOut!!!");
+    if (conflictOptOut.knowByDate) {
+      await pool.query(
+        `INSERT INTO wedding."ConflictsOptOut" (code, partymembers, elaboration, knowbydate) VALUES ('${
+          data.code
+        }', '{${
+          conflictOptOut.partyMembers
+        }}', '${conflictOptOut.elaboration.replace(/'/g, "")}', '${
+          conflictOptOut.knowByDate[2]
+        }-${conflictOptOut.knowByDate[0]}-${conflictOptOut.knowByDate[1]}')`
+      );
+    } else {
+      await pool.query(
+        `INSERT INTO wedding."ConflictsOptOut" (code, partymembers, elaboration, knowbydate) VALUES ('${data.code}', 'null', 'null', 'null')`
+      );
+    }
+    await pool.query("COMMIT");
+  } catch (error) {
+    console.log(error);
+    res.json({ result: false });
+  }
+
+  /* Questionaire!!!*/
+  try {
+    const questionaire = data.questionnaire;
+    await pool.query(
+      `INSERT INTO wedding."Questionnaire"(
+        code, staywith, needquiet, wholetime, rentalcar, changedlocation, generalcomment)
+        VALUES ('${data.code}', '${questionaire.stayWith}','${questionaire.needQuiet}', '${questionaire.wholeTime}','${questionaire.rentalCar}', '${questionaire.changedLocation}','${questionaire.generalComment}');`
+    );
+    await pool.query("COMMIT");
+  } catch (error) {
+    console.log(error);
+    res.json({ result: false });
+  }
+
+  /* Conflicts*/
+  try {
+    const conflicts = data.conflicts.conflictsArray;
+    for (const conflict of conflicts) {
+      console.log(conflict);
+      await pool.query(
+        `INSERT INTO wedding."Conflicts"(
+        code, partymembers, description, startsondate, endsondate)
+        VALUES ('${data.code}', '{${conflict.partyMembers}}', '${
+          conflict.description
+        }', '${conflict.startsOnDate[2]}-${conflict.startsOnDate[0]}-${
+          conflict.startsOnDate[1]
+        }', '${conflict.endsOnDate[2]}-${conflict.endsOnDate[0]}-${
+          conflict.endsOnDate[1]
+        }');`
+      );
+    }
+    await pool.query("COMMIT");
+  } catch (error) {
+    console.log(error);
+    res.json({ result: false });
+  }
+
+  res.json({ result: true });
 });
 
 module.exports = router;
