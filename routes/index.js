d45d23b173e6b813fd164c422328e401ccce9d3e
Rachel LaViola <rachel.laviola@gmail.com>
2019-11-10 18:50:27 -0600

    Fixed many bugs.

--- routes/index.js
+++ routes/index.js
@@ -18,34 +18,36 @@ router.post("/login", async function(req, res, next) {
       `SELECT code FROM wedding."RSVP" WHERE code = '${req.body.code}'`
     );
     if (dbRes.rows.length && dbRes.rows[0]) {
-      res.json({
+      res.write(JSON.stringify({
         result: false,
         message: 'ALREADY_SUBMITTED'
-      });
+      }));
     }
     else {
       let partydbRes = await pool.query(
         `SELECT party FROM wedding."Parties" WHERE code = '${req.body.code}'`
       );
       if (partydbRes.rows.length && partydbRes.rows[0].party){
-        res.json({
+        res.write(JSON.stringify({
           result: true,
           partyMembers: partydbRes.rows[0].party
-        });
+        }));
       }
       else {
-        res.json({
+        res.write(JSON.stringify({
           result: false,
           message: "INVALID_CODE"
-        });
+        }));
       }
     }
   } catch(error) {
-    res.json({
+    res.write(JSON.stringify({
       result: false,
       message: "ERROR",
       error: error
-    });
+    }));
+  } finally {
+    res.end();
   }
 });
 
@@ -58,6 +60,7 @@ router.post("/submitRSVP", async function(req, res, next) {
     const yesList = [];
     const maybeList = [];
     const noList = [];
+    const sanElab = data.party.elaboration.replace(/'/g, "")
     for (const member of data.party.members) {
       member.status === "coming" ? yesList.push(member.name) : "";
       member.status === "maybe" ? maybeList.push(member.name) : "";
@@ -65,12 +68,12 @@ router.post("/submitRSVP", async function(req, res, next) {
     }
 
     await pool.query(
-      `INSERT INTO wedding."RSVP" (code, coming, maybe, probablyNot) VALUES ('${data.code}','{${yesList}}','{${maybeList}}','{${noList}}')`
+      `INSERT INTO wedding."RSVP" (code, coming, maybe, probablyNot, elaboration) VALUES ('${data.code}','{${yesList}}','{${maybeList}}','{${noList}}', '${sanElab}')`
     );
     await pool.query("COMMIT");
   } catch (error) {
-    console.log(error);
-    res.json({ result: false });
+    console.log(error, "RSVP");
+    res.write(JSON.stringify({ result: false }));
   }
   /* Save Vote */
   try {
@@ -87,58 +90,61 @@ router.post("/submitRSVP", async function(req, res, next) {
     );
     await pool.query("COMMIT");
   } catch (error) {
-    console.log(error);
-    res.json({ result: false });
+    console.log(error, "save vote");
+    res.write(JSON.stringify({ result: false }));
   }
   /* Conflict Opt Out*/
   try {
     const conflictOptOut = data.conflicts.conflictsOptOut;
-    console.log(conflictOptOut, "conflictOptOut!!!");
-    if (conflictOptOut.knowByDate) {
+    let partyMembers = conflictOptOut.partyMembers.length === 0 ? 'null' : conflictOptOut.partyMembers;
+    let elaboration = conflictOptOut.elaboration === null || conflictOptOut.elaboration === '' ? 'null' : conflictOptOut.elaboration.replace(/'/g, "");
+    let knowByDate = conflictOptOut.knowByDate.length === 0 ? null : conflictOptOut.knowByDate;
+    if(knowByDate !== null) {
       await pool.query(
         `INSERT INTO wedding."ConflictsOptOut" (code, partymembers, elaboration, knowbydate) VALUES ('${
           data.code
         }', '{${
-          conflictOptOut.partyMembers
-        }}', '${conflictOptOut.elaboration.replace(/'/g, "")}', '${
-          conflictOptOut.knowByDate[2]
-        }-${conflictOptOut.knowByDate[0]}-${conflictOptOut.knowByDate[1]}')`
+          partyMembers
+        }}', '${elaboration}', '${
+          knowByDate[2]
+        }-${knowByDate[0]}-${knowByDate[1]}')`
       );
     } else {
       await pool.query(
-        `INSERT INTO wedding."ConflictsOptOut" (code, partymembers, elaboration, knowbydate) VALUES ('${data.code}', 'null', 'null', 'null')`
+        `INSERT INTO wedding."ConflictsOptOut" (code, partymembers, elaboration, knowbydate) VALUES ('${data.code}', '{${partyMembers}}', '${elaboration}', '1900-01-01')`
       );
     }
     await pool.query("COMMIT");
   } catch (error) {
-    console.log(error);
-    res.json({ result: false });
+    console.log(error, "conflict output");
+    res.write(JSON.stringify({ result: false }));
   }
 
   /* Questionaire!!!*/
   try {
-    const questionaire = data.questionnaire;
+    const questionnaire = data.questionnaire;
+    let generalComment = questionnaire.generalComment === null || questionnaire.generalComment === '' ? 'null' : questionnaire.generalComment.replace(/'/g, "");
     await pool.query(
       `INSERT INTO wedding."Questionnaire"(
         code, staywith, needquiet, wholetime, rentalcar, changedlocation, generalcomment)
-        VALUES ('${data.code}', '${questionaire.stayWith}','${questionaire.needQuiet}', '${questionaire.wholeTime}','${questionaire.rentalCar}', '${questionaire.changedLocation}','${questionaire.generalComment}');`
+        VALUES ('${data.code}', '${questionnaire.stayWith}','${questionnaire.needQuiet}', '${questionnaire.wholeTime}','${questionnaire.rentalCar}', '${questionnaire.changedLocation}','${generalComment}');`
     );
     await pool.query("COMMIT");
   } catch (error) {
-    console.log(error);
-    res.json({ result: false });
+    console.log(error, "questionaire");
+    res.write(JSON.stringify({ result: false }));
   }
 
   /* Conflicts*/
   try {
     const conflicts = data.conflicts.conflictsArray;
     for (const conflict of conflicts) {
-      console.log(conflict);
+      let description = conflict.description === null || conflict.description === '' ? 'null' : conflict.description.replace(/'/g, "");
       await pool.query(
         `INSERT INTO wedding."Conflicts"(
         code, partymembers, description, startsondate, endsondate)
         VALUES ('${data.code}', '{${conflict.partyMembers}}', '${
-          conflict.description
+          description
         }', '${conflict.startsOnDate[2]}-${conflict.startsOnDate[0]}-${
           conflict.startsOnDate[1]
         }', '${conflict.endsOnDate[2]}-${conflict.endsOnDate[0]}-${
@@ -148,11 +154,12 @@ router.post("/submitRSVP", async function(req, res, next) {
     }
     await pool.query("COMMIT");
   } catch (error) {
-    console.log(error);
-    res.json({ result: false });
+    console.log(error, "conflicts");
+    res.write(JSON.stringify({ result: false }));
   }
 
-  res.json({ result: true });
+  res.write(JSON.stringify({ result: true }));
+  res.end();
 });
 
 module.exports = router;
